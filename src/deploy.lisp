d89993f81258c734a592bea385d74ab5e0facd6e
Dwight Spencer <dwightaspencer@gmail.com>
2026-08-18 17:38:25 +0000

    Add org.cispec Labels and decommission defprop
    Container units:
      org.cispec.application, org.cispec.managed-by, org.cispec.fqdn,
      org.cispec.service-account Labels added to every container section,
      following the denzuko org cispec.org CMDB identity standard.
    
    src/deploy.lisp:
      decommissioned :posix defprop - ordered teardown:
        1. Stop all containers (systemctl --user stop --all)
        2. rm /etc/haproxy/conf.d/<vhost>.cfg && systemctl reload haproxy
        3. loginctl terminate-user <account>
        4. loginctl disable-linger <account>
        5. userdel <account>
        6. zfs destroy -r <dataset> (each, irreversible)
        7. rm /etc/zfs-keys/<dataset>.key (each)

--- src/deploy.lisp
+++ src/deploy.lisp
@@ -28,6 +28,7 @@
            :service-account-uid
            :quadlets-written
            :haproxy-vhost-written
+           :decommissioned
            :invidious-container-sections
            :invidious-db-container-sections
            :invidious-network-sections
@@ -233,7 +234,11 @@ admins:~%  - denzuko~%registration_enabled: false~%login_enabled: true~%statisti
                       ("Volume"        . ,(format nil "~A:/invidious/config/config.yml:ro,Z"
                                                   config-path))
                       ("Network"       . "invidious.network")
-                      ("Label"         . "io.containers.autoupdate=registry")))
+                      ("Label"         . "io.containers.autoupdate=registry")
+                      ("Label"           . "org.cispec.application=watch-dapla-deploy")
+                      ("Label"           . "org.cispec.managed-by=consfigurator")
+                      ("Label"           . "org.cispec.fqdn=watch.dapla.net")
+                      ("Label"           . "org.cispec.service-account=invidious")))
       ("Service" . (("Restart"         . "on-failure")
                     ("TimeoutStartSec" . "120")
                     ("TimeoutStopSec"  . "30")))
@@ -345,6 +350,32 @@ backend ~A_be
   (quadlets-written *service-user* *home-mountpoint* *data-mountpoint* *config-path*)
   (quadlets-activated *service-user*)
   (haproxy-vhost-written))
+
+
+(defprop decommissioned :posix (user)
+  "Tear down the watch-dapla-deploy stack in least-destructive-first order.
+   Steps:
+     1. Stop all containers in the service account session.
+     2. Remove the HAProxy vhost config and reload HAProxy.
+     3. Terminate the service account login session.
+     4. Disable linger so the account session does not restart.
+     5. Delete the service account.
+     6. Destroy all ZFS datasets (irreversible without a backup).
+     7. Remove the ZFS encryption key files.
+   Confirm a current rsync.net replica or snapshot exists before
+   executing steps 6 and 7."
+  (:desc (format nil "watch-dapla-deploy decommissioned for ~~A" user))
+  (:apply
+   (mrun (format nil "machinectl shell ~~A@ /usr/bin/systemctl --user stop --all" user))
+   (mrun "rm" "-f" (format nil "/etc/haproxy/conf.d/~~A.cfg" *haproxy-vhost-name*))
+   (mrun "systemctl" "reload" "haproxy")
+   (mrun "loginctl" "terminate-user" user)
+   (mrun "loginctl" "disable-linger" user)
+   (mrun "userdel" user)
+   (mrun "zfs" "destroy" "-r" 'storage/users/invidious')
+   (mrun "zfs" "destroy" "-r" 'storage/containers/invidious')
+   (mrun "rm" "-f" '/etc/zfs-keys/invidious-users.key')
+   (mrun "rm" "-f" '/etc/zfs-keys/invidious-containers.key')))
 
 (defun deploy-app ()
   "Provision the Invidious stack via INVIDIOUS-HOST (Consfigurator, :local
